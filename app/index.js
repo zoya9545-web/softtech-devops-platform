5050595dde72828c6111f7fa8b27c81d2e5c2e40
zoya9545-web <zoya9545@gmail.com>
2026-06-15 08:40:36 +0530

    Added professional website design

--- app/index.js
+++ app/index.js
@@ -1,11 +1,72 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to SoftTech DevOps Platform");
+app.get('/', (req, res) => {
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>SoftTech DevOps Platform</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background: linear-gradient(to right, #1e3c72, #2a5298);
+                color: white;
+                text-align: center;
+                padding-top: 100px;
+            }
+
+            .container {
+                background: rgba(255,255,255,0.1);
+                padding: 40px;
+                border-radius: 15px;
+                width: 60%;
+                margin: auto;
+            }
+
+            h1 {
+                font-size: 45px;
+            }
+
+            p {
+                font-size: 22px;
+            }
+
+            .btn {
+                background: #28a745;
+                color: white;
+                padding: 15px 30px;
+                border: none;
+                border-radius: 8px;
+                text-decoration: none;
+                font-size: 18px;
+            }
+
+            .btn:hover {
+                background: #218838;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>🚀 SoftTech DevOps Platform</h1>
+
+            <p>
+                Real-Time CI/CD Pipeline using
+                GitHub, Jenkins, Docker and Kubernetes
+            </p>
+
+            <h3>DevOps Project by Asif</h3>
+
+            <br>
+
+            <a href="#" class="btn">Learn More</a>
+        </div>
+    </body>
+    </html>
+    `);
 });
 
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+    console.log('Server running on port 3000');
 });
